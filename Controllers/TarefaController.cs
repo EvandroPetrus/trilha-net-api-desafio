e2733eae8b700d92bfb202c7898bd52bdc9f5655
Evandro Petrus <evandro_petrus@outlook.com>
2022-09-16 19:41:50 -0300

    Connected EF-DB

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -17,18 +17,23 @@ namespace TrilhaApiDesafio.Controllers
 
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
-        {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+        {            
+            var tarefa = _context.Tarefas.Find(id);
+            
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            
+            var tarefas = _context.Tarefas.ToList();
+            return Ok(_context.Tarefas);
         }
 
         [HttpGet("ObterPorTitulo")]
@@ -61,7 +66,10 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
+
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
